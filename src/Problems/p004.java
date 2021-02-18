bb4207a3f9d8821aacc805b6297df8724dd47243
Manuel Schierenberg <mail@elanum.de>
2021-02-18 12:59:31 +0100

    feat(p004): :sparkles: solve Problem 4

---
+++ src/Problems/p004.java
@@ -0,0 +1,33 @@
+package src.Problems;
+import src.Helper.*;
+
+/**
+ * Largest palindrome product
+ */
+
+public class p004 {
+  public static void main(String[] args) {
+    int result = solve(3);
+    System.out.println("Result: " + result);
+  }
+
+  public static int solve(int digits) {
+    if (digits < 1) {
+      throw new IllegalArgumentException();
+    }
+    
+    int palindrome = -1;
+    int min = (int) Math.pow(10, digits - 1);
+    int max = (int) Math.pow(10, digits);
+
+    for (int i = min; i < max; i++) {
+      for (int j = min; j < max; j++) {
+        int prod = i * j;
+        if (Helper.isPalindrome(prod) && prod > palindrome) {
+          palindrome = prod;
+        }
+      }
+    }
+    return palindrome;
+  }
+}
