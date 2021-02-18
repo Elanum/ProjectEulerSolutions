e71699f86145c43e6d6ce11cfc8dfbde77935fbb
Manuel Schierenberg <mail@elanum.de>
2021-02-18 13:03:07 +0100

    feat(p005): :sparkles: solve Problem 5

---
+++ src/Problems/p005.java
@@ -0,0 +1,21 @@
+package src.Problems;
+import src.Helper.*;
+
+/**
+ * Smallest multiple
+ */
+
+public class p005 {
+  public static void main(String[] args) {
+    int result = solve(20);
+    System.out.println("Result: " + result);
+  }
+
+  public static int solve(int max) {
+    int result = 1;
+    for (int i = 1; i <= max; i++) {
+      result = Helper.lcm(i, result);
+    }
+    return result;
+  }
+}
